12596e8d25012c4a648eecf04054566069306ad4
chalimmm <39824546+chalimmm@users.noreply.github.com>
2020-05-09 13:42:52 +0700

    Create Week2_Selection.cpp

---
+++ Week2_Selection.cpp
@@ -0,0 +1,26 @@
+#include<cstdio>
+int support;
+int main(){
+	scanf("%d", &support);
+	printf(support > 0 ?
+		"Kamu gak sendiri, kok!":
+		"Kasian deh sendirian");
+}
+/*
+Selection statement
+memungkinkan suatu program untuk
+menguji beberapa kondisi, dan menjalankan
+perintah berdasarkan pada kondisi yang terpenuhi.
+Bentuknya bisa berupa:
+- if else
+- switch case
+- dan lainnya
+Seperti pada soal yang menggunakan logical expression.
+
+FYI:
+Dalam hidup, kita dihadapkan dengan banyak pilihan
+yang membuat pikiran kita bercabang.
+Namun, kita bisa melewatinya karena kecanggihan
+komputer buatan Allah ini yang disebut otak.
+MasyaAllah
+*/
