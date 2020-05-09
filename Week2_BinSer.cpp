12198f86d06a25870dcd28e30642f1a50b3de3b2
chalimmm <39824546+chalimmm@users.noreply.github.com>
2020-05-09 20:41:44 +0700

    Update Week2_BinSer.cpp

--- Week2_BinSer.cpp
+++ Week2_BinSer.cpp
@@ -4,30 +4,28 @@ using namespace std;
 int val[2000001],x;
 int ans,hit,n,a[101];
 
-int main ()
-{
-	scanf("%d",&n);
-	x=1;
+int main (){
+	scanf("%d",&n);	//n = Banyak Set
+	id=1;
 	ans=0;
 	hit=0;
-	for(int i=1;i<=n;i++)
-	{
-		scanf("%d",&a[i]);
+	for(int i=1;i<=n;i++){
+		scanf("%d",&a[i]); //a[i] adalah anggota Set
 	}
-	for(int i=1;i<=n;i++) if(a[i])
-		for(int j=1;j<=n;j++)
-			for(int k=1;k<=n;k++)
-			{
-				val[x]=a[i]*(a[j]+a[k]);
-				x++;
-			}
-	sort(val+1,val+x);
-	for(int i=1;i<=n;i++)
-		for(int j=1;j<=n;j++)
-			for(int k=1;k<=n;k++)
-			{
-				ans=a[i]*a[j]+a[k];
-				hit+=upper_bound(val+1,val+x,ans)-lower_bound(val+1,val+x,ans);
+	for(int i=1;i<=n;i++){
+		if(a[i]){ //a[i] merepresentasikan "d"
+			for(int j=1;j<=n;j++) //a[j] merepresentasikan "e"
+				for(int k=1;k<=n;k++){ //a[k] merepresentasikan "f"
+					val[++id]=a[i]*(a[j]+a[k]); //val[x] merepresentasikan "d*(e+f)"
+				}
+		}
+	}
+	sort(val+1,val+x); //Binary Search mencari pada data terurut
+	for(int x=1;x<=n;x++) //a[x] merepresentasikan "a"
+		for(int y=1;y<=n;y++) //a[y] merepresentasikan "b"
+			for(int z=1;z<=n;z++){ //a[x] merepresentasikan "c"
+				ans=a[x]*a[y]+a[z]; //ans merepresentasikan "a*b+c"
+				hit+=upper_bound(val+1,val+id,ans)-lower_bound(val+1,val+id,ans); //di sinilah Binary Search terjadi
 			}
 	printf("%d\n",hit);
 }
